93c01eac9315e432b9582fcf8189c16f9c365793
rahulsaikia19 <sdeatflipkart@gmail.com>
2026-09-15 20:39:15 +0530

    Time: 18 ms (14.39%), Space: 48.9 MB (13.06%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,33 +1,50 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
+    public int[] nextGreaterElements(int[] arr) {
 
-        int n = nums.length;
+        int n = arr.length;
+
+        Stack<Integer> s = new Stack<>();
+        ArrayList<Integer> v = new ArrayList<>();
+
+        for (int p = 2 * n - 1; p >= 0; p--) {
+
+            int i = p % n;
+
+            if (s.size() == 0) {
+                if (p < n) {
+                    v.add(-1);
+                }
+            }
+
+            else if (s.peek() > arr[i]) {
+                if (p < n) {
+                    v.add(s.peek());
+                }
+            }
+
+            else {
+                while (s.size() > 0 && s.peek() <= arr[i]) {
+                    s.pop();
+                }
+
+                if (p < n) {
+                    if (s.size() == 0) {
+                        v.add(-1);
+                    } else {
+                        v.add(s.peek());
+                    }
+                }
+            }
+
+            s.push(arr[i]);
+        }
+
+        Collections.reverse(v);
 
         int[] ans = new int[n];
-        Arrays.fill(ans, -1);
 
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 2 * n - 1; i >= 0; i--) {
-
-            int curr = nums[i % n];
-
-            while (!st.isEmpty() && st.peek() <= curr) {
-                st.pop();
-            }
-
-            if (i < n) {
-
-                if (st.isEmpty()) {
-                    ans[i] = -1;
-                }
-
-                else {
-                    ans[i] = st.peek();
-                }
-            }
-
-            st.push(curr);
+        for (int i = 0; i < n; i++) {
+            ans[i] = v.get(i);
         }
 
         return ans;
